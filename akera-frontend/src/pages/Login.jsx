9fad8de0f40b379422d8148d458f16f6751a4e4e
Brehyma Traore <brehymatraore50@gmail.com>
2026-01-06 15:36:35 +0000

    init frontend + login page

---
+++ akera-frontend/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+function Login() {
+  return (
+    <div>
+      <h2>Connexion</h2>
+
+      <form>
+        <input type="text" placeholder="Email ou téléphone" />
+        <br />
+        <input type="password" placeholder="Mot de passe" />
+        <br />
+      </form>
+    </div>
+  );
+}
+
+export default Login;
